23f3b6c9f387aa7b98441e937ed30d68323a467f
Jeff Hoefs <jeff.hoefs@gmail.com>
2015-04-25 16:35:40 -0700

    breaking out to new library

---
+++ examples/SimpleFirmataEncoder/SimpleEncoderFirmata.ino
@@ -0,0 +1,57 @@
+/*
+  Copyright (C) 2014 Nicolas Panel. All rights reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  See file LICENSE.txt for further informations on licensing terms.
+ */
+
+#include <ConfigurableFirmata.h>
+#include <FirmataExt.h>
+#include <FirmataReporting.h>
+#include "FirmataEncoder.h"
+
+FirmataExt firmataExt;
+FirmataReporting reporting;
+FirmataEncoder encoder;
+
+/*==============================================================================
+ * FUNCTIONS
+ *============================================================================*/
+void systemResetCallback()
+{
+  firmataExt.reset();
+}
+
+/*==============================================================================
+ * SETUP()
+ *============================================================================*/
+void setup()
+{
+  Firmata.setFirmwareVersion(FIRMATA_MAJOR_VERSION, FIRMATA_MINOR_VERSION);
+  firmataExt.addFeature(reporting);
+  firmataExt.addFeature(encoder);
+  /* systemResetCallback is declared here (in ConfigurableFirmata.ino) */
+  Firmata.attach(SYSTEM_RESET, systemResetCallback);
+  Firmata.begin(57600);
+  systemResetCallback();  // reset to default config
+}
+
+/*==============================================================================
+ * LOOP()
+ *============================================================================*/
+void loop()
+{
+  /* STREAMREAD - processing incoming messagse as soon as possible, while still
+   * checking digital inputs.  */
+  while (Firmata.available()) {
+    Firmata.processInput();
+  }
+
+  if (reporting.elapsed()) {
+    encoder.report();
+  }
+}
---
+++ readme.md
@@ -0,0 +1,13 @@
+#FirmataEncoder
+
+A Firmata wrapper for the [Encoder](https://www.pjrc.com/teensy/td_libs_Encoder.html) library.
+
+This library adds support for encoders to ConfigurableFirmata.
+
+##Installation
+
+1. Make sure you have the following dependencies installed in your `/Arduino/libraries/` folder:
+  - [ConfigurableFirmata](https://github.com/firmata/ConfigurableFirmata)
+  - [Encoder](https://www.pjrc.com/teensy/td_libs_Encoder.html)
+
+2. Clone or download and copy FirmataEncoder into your `/Arduino/libraries/` folder.
